b108d419d9fef16e0316859680421dc122e8a1e0
Yousef Dawood <yousefdawood31@gmail.com>
2026-04-16 15:28:28 +0200

    add createUserSchema and enhance userSchema validation rules

--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -2,16 +2,18 @@ import z from 'zod';
 
 export const userSchema = z.object({
   firstName: z
-    .string()
-    .min(1, 'First name is required')
+    .string({ error: 'First name is required' })
+    .min(3, 'First name must be at least 3 characters long')
     .max(255, 'First name must not exceed 255 characters'),
+
   lastName: z
-    .string()
-    .min(1, 'Last name is required')
+    .string({ error: 'Last name is required' })
+    .min(3, 'Last name must be at least 3 characters long')
     .max(255, 'Last name must not exceed 255 characters'),
+
   password: z
     .union([z.string(), z.number()])
-    .refine((val) => val.toString().length >= 8, {
+    .refine((val) => val.toString().length >= 10, {
       error: 'Password must be at least 10 characters long',
     }),
 });
