31db621093e34cfe1616952b818ed78c6cd62f68
Yousef Dawood <yousefdawood31@gmail.com>
2026-04-16 15:42:34 +0200

    correct typo in handleZodErrors function

--- src/common/utils/handleZodErrors.ts
+++ src/common/utils/handleZodErrors.ts
@@ -1,12 +1,12 @@
 import z from 'zod';
 
 export const handleZodErrors = function (zodError: z.ZodError) {
-  const errorOjbects = {};
+  const errorObjects = {};
 
   Object.entries(z.flattenError<unknown>(zodError).fieldErrors).forEach(
     (fieldError) =>
-      (errorOjbects[fieldError[0]] = (fieldError[1] as string[])[0]), // to get first error from the zod error
+      (errorObjects[fieldError[0]] = (fieldError[1] as string[])[0]), // to get first error from the zod error
   );
 
-  return { fieldErrors: errorOjbects };
+  return { fieldErrors: errorObjects };
 };
