38e98369792aab256eb7c4c20ede3092f507953b
Yousef Dawood <yousefdawood31@gmail.com>
2026-04-24 06:30:50 +0300

    add TransformInterceptor to handle response transformation

---
+++ src/common/interceptors/transform.interceptor.ts
@@ -0,0 +1,14 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class TransformInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle();
+  }
+}
