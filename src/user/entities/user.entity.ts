dc314f9794e342359524a6b531f9cc6a05887c7a
Yousef Dawood <yousefdawood31@gmail.com>
2026-05-02 08:43:32 +0300

    serialize the lastName and add the fullName field if the lastName exists

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,6 +1,6 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { Name } from './name';
-import { Exclude } from 'class-transformer';
+import { Exclude, Expose, Transform } from 'class-transformer';
 
 @Entity()
 export class User {
@@ -10,10 +10,21 @@ export class User {
   @Column({ unique: true })
   email: string;
 
+  @Transform(({ value }: { value: { first: string; last?: string } }) =>
+    value.last ? value : { first: value.first },
+  )
   @Column(() => Name, { prefix: false })
   name: Name;
 
   @Exclude()
   @Column()
   password: string;
+
+  @Expose()
+  get fullName(): string | void {
+    // prettier-ignore
+    if (!this.name.last)
+      return;
+    return `${this.name.first} ${this.name.last}`;
+  }
 }
