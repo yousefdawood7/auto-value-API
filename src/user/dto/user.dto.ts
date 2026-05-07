6fabbc2472501b19bbeec4d7734d361c5e6be3c3
Yousef Dawood <yousefdawood31@gmail.com>
2026-05-07 18:01:05 +0300

    refactor CreateUserDto to extend UserDto and remove redundant properties

---
+++ src/user/dto/user.dto.ts
@@ -0,0 +1,20 @@
+import { Exclude, Expose, Transform } from 'class-transformer';
+
+export class UserDto {
+  email: string;
+  firstName: string;
+
+  @Transform(({ value }: { value?: string }) => value ?? undefined)
+  lastName?: string;
+
+  @Exclude()
+  password: string;
+
+  @Expose()
+  fullName?() {
+    // prettier-ignore
+    if (!this.lastName)
+      return;
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
