995e72fa250b467f5d1529d4c9a87b49358fb83b
Yousef Dawood <yousefdawood31@gmail.com>
2026-05-30 22:21:48 +0300

    create reports resource and wire them up

---
+++ src/reports/report.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Post } from '@nestjs/common';
+import { ReportService } from './report.service';
+
+@Controller('/report')
+export class ReportController {
+  constructor(private readonly reportService: ReportService) {}
+
+  @Post()
+  createReport() {}
+}
