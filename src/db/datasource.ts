b7120139eacef063a0d55c9b5aeeb3808c1533a5
Yousef Dawood <yousefdawood31@gmail.com>
2026-06-08 11:26:34 +0300

    add PostgreSQL datasource configuration and migration scripts

--- src/db/datasource.ts
+++ src/db/datasource.ts
@@ -1,6 +1,7 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import 'dotenv/config';
 import { ConfigService } from '@nestjs/config';
+import path from 'node:path';
 
 const configService = new ConfigService();
 
@@ -8,8 +9,9 @@ export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
   ssl: true,
   url: configService.getOrThrow<string>('DATABASE_URL'),
-  entities: ['dist/**/*.entity.js'],
-  migrations: ['dist/db/migrations/*.js'],
+  entities: [path.join(__dirname, '..', '**', '*.entity.{ts,js}')],
+  migrations: [__dirname + '/migrations/**/*{.js,.ts}'],
+
   migrationsTableName: 'migrations',
   migrationsRun: false,
   synchronize: false,
