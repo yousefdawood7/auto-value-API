6f79ffd44def03e0f373e82019fb2d7b655f6487
Yousef Dawood <yousefdawood31@gmail.com>
2026-06-08 10:37:26 +0300

    migrate database from SQLite to PostgreSQL with async TypeORM configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,11 +17,15 @@ import { ReportModule } from './reports/report.module';
       isGlobal: true,
       cache: true,
     }),
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [__dirname + '/**/*.entity.{ts,js}'],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        ssl: true,
+        url: configService.getOrThrow<string>('DATABASE_URL'),
+        entities: [__dirname + '/**/*.entity.{ts,js}'],
+        synchronize: false,
+      }),
+      inject: [ConfigService],
     }),
     UserModule,
     ReportModule,
